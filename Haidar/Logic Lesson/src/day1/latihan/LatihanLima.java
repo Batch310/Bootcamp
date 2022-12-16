9f958921dfe3d986d40343d147c052b927c18c78
hamirf <haidar300400@gmail.com>
2022-12-16 16:53:19 +0700

    Latihan Soal Fix

--- Haidar/Logic Lesson/src/day1/latihan/LatihanLima.java
+++ Haidar/Logic Lesson/src/day1/latihan/LatihanLima.java
@@ -6,18 +6,18 @@ public class LatihanLima {
 
 	public static void main(String[] args) {
 		
-		Scanner masukan = new Scanner(System.in);
+		Scanner masukan = new Scanner(System.in); //Inisiasi tipe data Scanner variabel masukan sebagai input dari console
 		
 		System.out.print("Banyaknya uang sepuluh ribuan:  ");
-		int jsepuluh = masukan.nextInt();
+		int jsepuluh = masukan.nextInt(); //Input banyaknya uang sepuluh ribuan
 		
 		System.out.print("Banyaknya uang lima ribuan: ");
-		int jlima = masukan.nextInt();
+		int jlima = masukan.nextInt(); //Input banyaknya uang lima ribuan
 		
 		System.out.print("Banyaknya uang dua ribuan: ");
-		int jdua = masukan.nextInt();
+		int jdua = masukan.nextInt(); //Input banyankya uang dua ribuan
 		
-		int total = (10000*jsepuluh)+(5000*jlima)+(2000*jdua);
+		int total = (10000*jsepuluh)+(5000*jlima)+(2000*jdua); //Rumus total uang yang dimiliki
 		
 		System.out.println("Total uang yang dimiliki pedagang adalah " + total);
 		
