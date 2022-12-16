e119437186a321efb4e416f20da96a1774b2f781
Imam Aditya <120548261+ImamAditya@users.noreply.github.com>
2022-12-16 15:34:37 +0700

    Latihan

---
+++ Imam/Logic/src/day1/latihan/Latihan4.java
@@ -0,0 +1,25 @@
+package day1.latihan;
+
+import java.util.Scanner;
+
+public class Latihan4 {
+
+	public static void main(String[] args) {
+		
+     Scanner inputan = new Scanner(System.in);
+     System.out.print("Harga Beli :");
+     int beli = inputan.nextInt();
+     
+     System.out.print("Harga Jual :");
+     int jual = inputan.nextInt();
+     
+//     double laba = ;
+     
+//     System.out.print("Keuntungan Pedagang adalah" + laba + "persen");
+     
+     
+     
+     
+	}
+
+}
