d621148293efd1697485a36e9cb2ada603b43983
Yuhananda Aditama <yuhananda.aditama@xsis.co.id>
2022-12-16 11:06:32 +0700

    Day 1
    123

---
+++ Trainer/Logic310/src/day1/pagi/MateriEmpat.java
@@ -0,0 +1,43 @@
+package day1.pagi;
+
+public class MateriEmpat {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+
+		// Kondisi satu rangakin if, else if, else
+//		if(a>=10) {
+//			//kalau kondisi terpenuhi apa yg dilakukan
+//			System.out.println("A bilangan besar");
+//		} else if(a == 10) {
+//			System.out.println("A adalah 10");
+//		} else {
+//			System.out.println("A bukan bilangan besar");
+//		}
+
+		// Kondisi berdiri sendiri if if if
+//		if (a < 5) {
+//			System.out.println("A lebih kecil dari 5");
+//		}
+//		
+//		if (a < 3) {
+//			System.out.println("A lebih kecil dari 3");
+//		}
+
+		// Kondisi Switch Case
+		switch (a) {
+		case 1:
+			System.out.println("A = 1");
+			break;
+		case 2:
+			System.out.println("A = 2");
+			break;
+		default:
+			System.out.println("A bukan siapa2");
+			break;
+		}
+
+	}
+
+}
---
+++ Trainer/Logic310/src/day1/pagi/MateriSatu.java
@@ -0,0 +1,20 @@
+package day1.pagi; //Nandain lokasi file java ini 
+
+public class MateriSatu { //Deklarasi Nama Class
+
+	public static void main(String[] args) {//Fungsi main/utama dari class
+		
+		//Cetak saja
+		System.out.print("Halo Dunia!");
+		
+		//Cuma Enter
+		//System.out.print("\n");
+		
+		//Cuma Enter
+		System.out.println();
+		
+		//Cetak lalu enter
+		System.out.println("Saya disini");
+	}
+
+}
