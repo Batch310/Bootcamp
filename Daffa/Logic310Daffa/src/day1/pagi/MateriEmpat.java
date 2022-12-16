eaf757464f611b729e43c9da870ca44830658d8b
masmahen <mahendradaffareza@gmail.com>
2022-12-16 11:07:45 +0700

    jambrong
    a

---
+++ Daffa/Logic310Daffa/src/day1/pagi/MateriEmpat.java
@@ -0,0 +1,35 @@
+package day1.pagi;
+
+public class MateriEmpat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+//		
+//		if(a > 10) {
+//			System.out.println("A bilangan besar");
+//		}
+//		else if(a == 10) {
+//			System.out.println("A adalah 10");
+//		}
+//		else {
+//			System.out.println("A bukan bilangan besar");
+//		}
+		
+		switch (a) {
+		case 1:
+			System.out.println("A = 1");
+			break;
+		case 2:
+			System.out.println("A = 2");
+			break;
+		default:
+			System.out.println("A bukan siapa-siapa");
+			break;
+		}
+		
+
+	}
+
+}
