50d326972526e2b230763e89ea247258978adc11
hamirf <haidar300400@gmail.com>
2022-12-16 11:10:46 +0700

    Day 1 Lesson
    Materi Dasar Java Hari Ke-1

---
+++ Haidar/Logic Lesson/src/day1/morning/MateriSatu.java
@@ -0,0 +1,18 @@
+package day1.morning; //Lokasi file java ini
+
+public class MateriSatu { //Deklarasi nama class atau file coding
+
+	public static void main(String[] args) { //Deklarasi fungsi main atau utama dari class, ini menandakan bahwa class ini nantinya bisa di run
+		
+		//Cetak Saja
+		System.out.print("Halo Dunia!");
+		
+		//Hanya enter
+		System.out.print("\n"); //membuat baris baru
+		System.out.println(); //membuat baris baru
+		
+		//Cetak lalu enter
+		System.out.println("Saya disini");
+	}
+
+}
