ac00198c51fb64d2e6d0946e4e4fa2b7b07683d4
arnandasw <arnandawibawa1114@gmail.com>
2022-12-16 16:50:21 +0700

    day1

--- Nanda/Logic310/src/day1/latihan/Latihan10.java
+++ Nanda/Logic310/src/day1/latihan/Latihan10.java
@@ -1,9 +1,22 @@
 package day1.latihan;
 
+import java.util.Scanner;
+
 public class Latihan10 {
 
 	public static void main(String[] args) {
+		Scanner inputan = new Scanner(System.in);
+		long PIN = 234567l;
 		
+		System.out.print("Masukkan PIN Anda : ");
+		int angka = inputan.nextInt();
+		
+		if (angka==PIN) {
+			System.out.print("Uang yang disetor : ");
+			long setor = inputan.nextLong();
+		} else {
+			System.out.println("PIN Salah");
+		}
 	}
 
 }
