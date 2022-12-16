7fb596a66d4b9566f9c18204f43889f3f218cc4d
masmahen <mahendradaffareza@gmail.com>
2022-12-16 11:46:57 +0700

    Latihan
    Jambrong

---
+++ Daffa/Logic310Daffa/src/day1/latihan/Latihan10.java
@@ -0,0 +1,15 @@
+package day1.latihan;
+
+public class Latihan10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Nama        : Bambang");
+		System.out.println("Umur        : 20");
+		System.out.println("Golongan A  : A");
+		System.out.println("Universitas : Universitas Xsis Academy");
+
+	}
+
+}
---
+++ Daffa/Logic310Daffa/src/day1/latihan/Latihan9.java
@@ -0,0 +1,10 @@
+package day1.latihan;
+
+public class Latihan9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
