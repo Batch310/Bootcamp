a10bfbf547a6afed5f23317fc70eb04e9681d201
Tyannn <tyanprisma00@gmail.com>
2022-12-16 11:14:42 +0700

    Latihan

---
+++ Tyannn/Logic310/src/day1/pagi/MateriDua.java
@@ -0,0 +1,27 @@
+package day1.pagi;
+
+public class MateriDua {
+
+	public static void main(String[] args) {
+		// DEKLARASI VARIABLE
+		int angka;
+		
+		angka =10;
+		angka =10 + 13;
+		System.out.println(angka);
+		
+		long angkaPanjang =20;
+		double angkaDesimal = 1.5;
+		double angkaDesimal2 = 2.3;
+		char sebuahChar = 'A';
+		boolean sebuahBoolean = true;
+		float angkaFloat = 2.5f;
+		
+		String nama = "Tyan";
+		
+		// Deklarasi class sendiri
+		MateriSatu namaVariabel;
+
+	}
+
+}
