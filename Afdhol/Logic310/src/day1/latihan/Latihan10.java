ba1323033cd0cd369e0cd18059ff0be2c0e9667d
Afdhol <afdholbasil@gmail.com>
2022-12-18 22:40:43 +0700

    lat10

--- Afdhol/Logic310/src/day1/latihan/Latihan10.java
+++ Afdhol/Logic310/src/day1/latihan/Latihan10.java
@@ -7,7 +7,7 @@ public class Latihan10 {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		int pin=123456, uang_saldo, pilihan, kode_bank, uang_setor = 0;
+		int pin=123456, uang_saldo, pilihan, kode_bank, uang_setor = 0, sisa_saldo=0;
 		String rekening;
 
 		System.out.print("Masukkan PIN : ");
@@ -29,6 +29,7 @@ public class Latihan10 {
 		if (pilihan == 1) {
 			System.out.println("Masukkan Rekening Tujuan : ");
 			rekening = input.next();
+			
 			if (rekening.length()==10) {
 				System.out.println("Uang disetor: " );
 				uang_setor = input.nextInt();
@@ -41,40 +42,32 @@ public class Latihan10 {
 				}
 			
 			}else {
-				System.out.println("saldo tidak cukup");
+				System.out.println("rekening minimal 10 digit");
 			}
 
 			
 
-		} else if (pilihan == 2) {
+		} if (pilihan == 2) {
 			System.out.print("Masukkan kode bank: ");
 			kode_bank = input.nextInt();
-
-			System.out.print("Masukkan Rekening Tujuan: ");
+			System.out.println("Masukkan Rekening Tujuan : ");
 			rekening = input.next();
+			
 			if (rekening.length()==10) {
 				System.out.println("Uang disetor: " );
 				uang_setor = input.nextInt();
-				
+
 				if ((uang_saldo-uang_setor-7500)<0) {
 					System.out.println("saldo ga cukup");
-
 				}else if (uang_saldo>0) {
-						System.out.println("Transaksi anda berhasil, saldo anda saat ini: " + (uang_saldo - 7500));
-					}
-				
+					System.out.println("Transaksi berhasil, saldo anda selama ini : " + (uang_saldo-uang_setor-7500));
+				}
+			
 			}else {
-				System.out.println("Panjang Rekening harus 10");
+				System.out.println("rekening minimal 10 digit");
 			}
 
-			
-			input.nextInt();
 
-			
-			
-		} else {
-			
-			System.out.println("pilihan anda salah");
 		}
 
 	}
