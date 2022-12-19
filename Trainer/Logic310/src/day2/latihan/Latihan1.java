d7c347d90ec95f93f824dcd05eb6924930b120e8
Yuhananda Aditama <yuhananda.aditama@xsis.co.id>
2022-12-19 15:58:40 +0700

    1
    1

---
+++ Trainer/Logic310/src/day2/latihan/Latihan1.java
@@ -0,0 +1,16 @@
+package day2.latihan;
+
+public class Latihan1 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		int nilaiAwal = 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(nilaiAwal+" ");
+			nilaiAwal = nilaiAwal + 2;
+		}
+
+	}
+
+}
