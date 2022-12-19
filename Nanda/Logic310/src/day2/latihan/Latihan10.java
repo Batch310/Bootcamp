33a7d1198e2f793f4447d39afad7f896aa72f865
arnandasw <arnandawibawa1114@gmail.com>
2022-12-19 15:32:55 +0700

    day2.latihan

--- Nanda/Logic310/src/day2/latihan/Latihan10.java
+++ Nanda/Logic310/src/day2/latihan/Latihan10.java
@@ -2,19 +2,19 @@ package day2.latihan;
 
 import java.util.Scanner;
 
-public class Latihan7 {
+public class Latihan10 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int n, awal=2;
+		int n, awal=3;
 		
 		System.out.print("n = ");
 		n = input.nextInt();
 		
 		for(int i=1; i<=n; i++) {
 			System.out.print(awal+ " ");
-			awal=awal*2;
+			awal=awal*3;
 		}
 	}
 
