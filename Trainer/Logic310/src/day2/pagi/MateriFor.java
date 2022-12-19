5a3c3ac86baddf66634ecaad531d922fe2a6234f
Yuhananda Aditama <yuhananda.aditama@xsis.co.id>
2022-12-19 11:19:49 +0700

    1

---
+++ Trainer/Logic310/src/day2/pagi/MateriFor.java
@@ -0,0 +1,29 @@
+package day2.pagi;
+
+public class MateriFor {
+	public static void main(String[] args) {
+		int i = 1;
+		//Contoh While
+		while(i <= 5) {
+			System.out.println("Yuhan");
+			i++; // i = i + 1
+		}
+		
+		System.out.println();
+		
+		//Contoh for, mirip while
+		int j = 1;
+		for(;j<=5;) {
+			System.out.println("Nanda");
+			j++;
+		}
+		
+		System.out.println();
+		
+		//Contoh for, ultimate form
+		for(int k = 1;k<=5;k++) {
+			System.out.println("Adi");
+		}
+		
+	}
+}
