444a0c7786a8dc0caa8cd10449110ac814f78932
Tyannn <tyanprisma00@gmail.com>
2022-12-19 16:49:36 +0700

    Day2LatihanFix

--- Tyannn/Logic310/src/day2/latihan/Day2Latihan10.java
+++ Tyannn/Logic310/src/day2/latihan/Day2Latihan10.java
@@ -1,9 +1,31 @@
 package day2.latihan;
 
+import java.util.Scanner;
+
 public class Day2Latihan10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("n = ");
+		int n = input.nextInt();
+
+		// n = 7
+		// 3 9 27 XXX 243 729 2187
+		// 0 1 2   3   4   5    6
+		
+		
+		int awal = 3;
+		for (int i = 0; i < n; i++) {
+
+			if (i == n/2) {
+				System.out.print("xxx ");
+			} else {
+				System.out.print(awal + " ");
+
+			}
+			awal *= 3;
+		}
 
 	}
 
