5ce1d7689b743c1d5dc3cea0a0693ebbea0da065
Panji Tektok <panjibernadinus@gmail.com>
2022-12-19 15:57:57 +0700

    day2latihan
    latihan

---
+++ Panji/Logic310/src/day2/latihan/Soal3.java
@@ -0,0 +1,22 @@
+package day2.latihan;
+
+import java.util.Scanner;
+
+public class Soal3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("n : ");
+		int n = input.nextInt();
+
+		int a = 1;
+		for (int i = 0; i < n; i++) {
+			System.out.print(a + " ");
+			a = a + 3;
+
+		}
+
+	}
+
+}
