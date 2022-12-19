592110e0b247f6a780218990bc20b7d4603e5090
Yudha Yuniartha Kusuma Wardhana <ykwyudha@gmail.com>
2022-12-19 11:20:44 +0700

    Yudha - Day 2 Pagi

---
+++ Yudha/Logic310/src/day2/pagi/MateriWhile.java
@@ -0,0 +1,26 @@
+package day2.pagi;
+
+public class MateriWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Perulangan cetak nama 5 kali
+		int i = 0;
+		while(i<5) {
+			System.out.println("yykw");
+			i++;
+		}
+		i=0;
+		do {
+			System.out.println("Yudha");
+			i++;
+		}while(i<5);
+		
+		for(i=0;i<5;i++) {
+			System.out.println("XSis Academy");
+		}
+		
+	}
+
+}
