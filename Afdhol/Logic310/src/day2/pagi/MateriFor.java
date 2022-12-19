1067398d7a96ea02c2348c3ee255b1a5ab5a16d0
Afdhol <afdholbasil@gmail.com>
2022-12-19 11:20:42 +0700

    materi2

---
+++ Afdhol/Logic310/src/day2/pagi/MateriFor.java
@@ -0,0 +1,25 @@
+package day2.pagi;
+
+public class MateriFor {
+
+	public static void main(String[] args) {
+
+		//FOR 
+		
+		//contoh for mirip while
+		int j = 1;
+		for(;j<=5;) {
+			System.out.println("Nanda");
+			j++;
+		}
+		
+		//for bentuk asli
+		
+		System.out.println();
+		
+		for(int k=1;k<=5;k--) {
+			System.out.println("Adi");
+		}
+	}
+
+}
