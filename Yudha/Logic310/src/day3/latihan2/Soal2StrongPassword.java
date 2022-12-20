3619dd101b610b7002d0addf11e010274131ea80
Yudha Yuniartha Kusuma Wardhana <ykwyudha@gmail.com>
2022-12-20 16:53:55 +0700

    Update day 3

--- Yudha/Logic310/src/day3/latihan2/Soal2StrongPassword.java
+++ Yudha/Logic310/src/day3/latihan2/Soal2StrongPassword.java
@@ -1,9 +1,20 @@
 package day3.latihan2;
 
+import java.util.Scanner;
+
 public class Soal2StrongPassword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String numbers = "0123456789";
+		String lower_case = "abcdefghijklmnopqrstuvwxyz";
+		String upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String special_characters = "!@#$%^&*()-+";
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Masukkan kalimat : ");
+		String password = input.next();
+		
 
 	}
 
