ef465b7c543d3e025d36f645f2a00541b120ecc6
arnandasw <arnandawibawa1114@gmail.com>
2022-12-20 16:33:42 +0700

    day3

--- Nanda/Logic310/src/day3/Latihan/Latihan9.java
+++ Nanda/Logic310/src/day3/Latihan/Latihan9.java
@@ -1,10 +1,41 @@
+
 package day3.Latihan;
+
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Latihan9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("n = ");
+		int n = input.nextInt();
+		
+		System.out.print("n2 = ");
+		int n2 = input.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(i+ " ");			
+		}
+		
+		System.out.println();
+		
+		int a = 0;
+		for (int i = 0; i < n; i++) {
+			System.out.print(a+ " ");
+			a+=n2;
+		}
+		
+		System.out.println();
+		
+		int b = 0;
+		for (int i = 0; i < n; i++) {
+			b-=n2;
+			System.out.print((b+i) + " ");
+			
+		}
+		
 	}
 
 }
