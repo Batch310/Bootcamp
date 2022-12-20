4df3fb2aec5058bd752ed69da7e95b6d9902599d
hamirf <haidar300400@gmail.com>
2022-12-20 09:41:04 +0700

    Latihan 2 Day 2 All Done
    Yeah

---
+++ Haidar/Logic Lesson/src/day2/latihan2/Latihan2Soal07.java
@@ -0,0 +1,38 @@
+package day2.latihan2;
+
+import java.util.Scanner;
+
+public class Latihan2Soal07 {
+	public static void main(String[] args) {
+		
+		Scanner masukan = new Scanner(System.in);
+		
+		System.out.print("Masukkan banyaknya deret: ");
+		int deret = masukan.nextInt();
+		
+		int angka = 0;
+		int[][] deretAngka = new int[deret][3];
+		
+		for (int i = 0; i < deretAngka.length; i++) {
+			deretAngka[i][0] = angka;
+			System.out.print(deretAngka[i][0] + " ");
+			angka++;
+		}
+		
+		System.out.println();
+		
+		for (int j = 0; j < deretAngka.length; j++) {
+			deretAngka[j][1] = angka;
+			System.out.print(deretAngka[j][1] + " ");
+			angka++;
+		}
+		
+		System.out.println();
+		
+		for (int k = 0; k < deretAngka.length; k++) {
+			deretAngka[k][2] = angka;
+			System.out.print(deretAngka[k][2] + " ");
+			angka++;
+		}
+	}
+}
