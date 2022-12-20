d76c13114816d50ce48f6d206548e91df10a4d01
hamirf <haidar300400@gmail.com>
2022-12-20 16:13:59 +0700

    A Very Big Sum Done
    Latihan warmup A Very Big Sum done

--- Haidar/Logic Lesson/src/day3/warmup/AVeryBigSum.java
+++ Haidar/Logic Lesson/src/day3/warmup/AVeryBigSum.java
@@ -1,5 +1,23 @@
 package day3.warmup;
 
-public class AVeryBigSum {
+import java.util.Scanner;
 
+public class AVeryBigSum {
+	public static void main(String[] args) {
+		
+		Scanner masukan = new Scanner(System.in);
+		
+		System.out.print("Masukkan banyaknya angka: ");
+		int input = masukan.nextInt();
+		
+		long sum = 0;
+		
+		for (int i = 0; i < input; i++) {
+			System.out.print("Masukkan angka besar: ");
+			long angka = masukan.nextLong();
+			sum += angka;
+		}		
+		System.out.println(sum);
+		
+	}
 }
