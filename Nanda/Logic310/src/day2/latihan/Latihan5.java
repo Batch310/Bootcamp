cb83548aa056b4356f2068e5e48fe178992ecb01
arnandasw <arnandawibawa1114@gmail.com>
2022-12-20 12:48:44 +0700

    day3

--- Nanda/Logic310/src/day2/latihan/Latihan5.java
+++ Nanda/Logic310/src/day2/latihan/Latihan5.java
@@ -14,8 +14,8 @@ public class Latihan5 {
 		
 		for(int i=1; i<=n; i++) {
 			//i:
-			//0,1,2,3,4,5,6
-			//0,1,*3,4,*,6
+			//,1,2,3,4,5,6,7
+			//,1,2,*,4,5,*,7
 			if(i%3==0) {
 				System.out.print("* ");
 			}else {
