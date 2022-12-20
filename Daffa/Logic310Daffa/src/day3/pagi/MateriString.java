bfa82429b4f8043d89632511d7bd2e0ea7c78996
masmahen <mahendradaffareza@gmail.com>
2022-12-20 08:33:20 +0700

    1

--- Daffa/Logic310Daffa/src/day3/pagi/MateriString.java
+++ Daffa/Logic310Daffa/src/day3/pagi/MateriString.java
@@ -6,21 +6,38 @@ public class MateriString {
 		// TODO Auto-generated method stub
 		String a = "Xsis Academy";
 		String b = "Xsis academy";
-		
+
 		// Fungsi "equals" untuk membandingkan String
 		if (a.equals(b)) {
-			System.out.println("Ya");
+			System.out.println("Sama persis");
 		} else {
-			System.out.println("Tidak");
-		}
-		
-		// Fungsi equalsIgnoreCase tidak case-sensitive
-		if (a.equalsIgnoreCase(b)) {
-			System.out.println("Ya");
-		} else {
-			System.out.println("Tidak");
+			System.out.println("Tidak sama persis");
 		}
 
+		// Fungsi equalsIgnoreCase tidak case-sensitive
+		if (a.equalsIgnoreCase(b)) {
+			System.out.println("Mirip");
+		} else {
+			System.out.println("Tidak mirip");
+		}
+
+		// Mendapatkan panjang String
+		System.out.println(a.length());
+
+		// Mendapatkan 1 karakter dalam String
+		char newChar = a.charAt(6);
+		System.out.println(a.charAt(6));
+
+		// Mengubah String menjadi huruf besar semua
+		System.out.println(a.toUpperCase());
+		
+		// Mengubah String menjadi huruf kecil semua
+		System.out.println(a.toLowerCase());
+		
+		// Fungsi replace untuk mengganti karakter dalam String
+		a = a.replace("Xsis", "Jambrong");
+		System.out.println(a);
+		System.out.println(a.replace(" ", "|||"));
 	}
 
 }
