dd3203dc2f01a0b22a9a4f907663a244747be542
hamirf <haidar300400@gmail.com>
2022-12-20 14:01:59 +0700

    Staircase Done
    Latihan warmup staircase done

---
+++ Haidar/Logic Lesson/src/day3/warmup/Staircase.java
@@ -0,0 +1,24 @@
+package day3.warmup;
+
+import java.util.Scanner;
+
+public class Staircase {
+	public static void main(String[] args) {
+		
+		Scanner masukan = new Scanner(System.in);
+		
+		System.out.print("Masukkan tinggi dan lebar tangga: ");
+		int anakTangga = masukan.nextInt();
+		
+		for (int i = 0; i < anakTangga; i++) {
+			for (int j = 0; j < anakTangga; j++) {
+				if (i + j < 5) {
+					System.out.print(" ");
+				} else {
+					System.out.print("#");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
