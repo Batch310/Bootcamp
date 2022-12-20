8623e04bab579a2670ff15e0ca4548db2d134790
Panji Tektok <panjibernadinus@gmail.com>
2022-12-20 13:35:53 +0700

    day3practice fix goal
    latihan 2dim

--- Panji/Logic310/src/day2/latihan2/Soal6.java
+++ Panji/Logic310/src/day2/latihan2/Soal6.java
@@ -9,19 +9,22 @@ public class Soal6 {
 		System.out.print("n : ");
 		int n = input.nextInt();
 
+
 		int a = 1;
 		int b = 1;
+		int c = 1;
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < n; j++) {
 				if (i == 0) {
 					System.out.print(j + " ");
 				} else if (i == 1) {
 					System.out.print(a + " ");
-					a = a * 7;
+					a = a * n;
 				} else {
-					System.out.print(b + " ");
-					
+					System.out.print(c + " ");
 					b = b * n;
+					int hasilB = b + 1;
+					c = hasilB + j;
 				}
 
 			}
