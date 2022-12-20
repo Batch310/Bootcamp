ed03f7c46c3385ef7f2ff7b9361a14c9c308d66e
Afdhol <afdholbasil@gmail.com>
2022-12-20 08:11:11 +0700

    ketinggalan

--- Afdhol/Logic310/src/day2/latihan/Soal6.java
+++ Afdhol/Logic310/src/day2/latihan/Soal6.java
@@ -6,30 +6,25 @@ public class Soal6 {
 
 	public static void main(String[] args) {
 
-		
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("input: ");
 		int angka = input.nextInt();
-		
-		
+
 		int awal = 1;
-		
-		
-		for(int i=0; i<angka; i++) {
-			
-			
-		if (awal%3==0) {
-			System.out.print("*"+" ");
-			
-			
-		}else {
-			System.out.print(awal+" ");
-		}
-		awal = awal+4;
+
+		for (int i = 0; i < angka; i++) {
+
+			if (awal % 3 == 0) {
+				System.out.print("*" + " ");
+
+			} else {
+				System.out.print(awal + " ");
+			}
+			awal = awal + 4;
 
 		}
-		
+
 	}
 
 }
