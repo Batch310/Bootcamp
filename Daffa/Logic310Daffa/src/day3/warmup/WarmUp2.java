5ef46695b88a26641c11e46e3e49a0995830cac0
masmahen <mahendradaffareza@gmail.com>
2022-12-20 21:08:03 +0700

    1
    1

--- Daffa/Logic310Daffa/src/day3/warmup/WarmUp2.java
+++ Daffa/Logic310Daffa/src/day3/warmup/WarmUp2.java
@@ -8,9 +8,23 @@ public class WarmUp2 {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		String waqtu = scan.next();
+		
 		String tArr[] = waqtu.split(":");
 		String AmPm = tArr[2].substring(2,4);
 		int jam, menit, detiq;
+		
+		jam = Integer.parseInt(tArr[0]);
+		menit = Integer.parseInt(tArr[1]);
+		detiq = Integer.parseInt(tArr[2].substring(0,2));
+		
+		String cekPM = "PM", cekAM = "AM";
+		
+		if (AmPm.equals(cekAM) && jam == 12) {
+			jam = 0;
+		} else if (AmPm.equals(cekPM) && jam == 24) {
+			jam += 12;
+		}
+		System.out.printf("%02d:%02d:%02d",jam,menit,detiq);
 	}
 
 }
