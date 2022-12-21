18c26f2142aa9b2107a92a0f6d589c8170baf6f9
masmahen <mahendradaffareza@gmail.com>
2022-12-21 13:23:03 +0700

    1
    1

--- Daffa/Logic310Daffa/src/day4/pagi/DiagonalDifference.java
+++ Daffa/Logic310Daffa/src/day4/pagi/DiagonalDifference.java
@@ -15,6 +15,7 @@ public class DiagonalDifference {
 
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print("Masukkan angka : ");
 				matrix[i][j] = scanner.nextInt();
 			}
 		}
