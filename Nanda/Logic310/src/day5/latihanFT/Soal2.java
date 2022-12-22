f90788f317b177572ca4a8c959de65ff778551dc
arnandasw <arnandawibawa1114@gmail.com>
2022-12-22 13:49:57 +0700

    day5

---
+++ Nanda/Logic310/src/day5/latihanFT/Soal2.java
@@ -0,0 +1,18 @@
+package day5.latihanFT;
+
+import java.util.Scanner;
+
+public class Soal2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Masukkan Kalimat : ");
+		String kalimat = input.next();
+		
+		String vokal = "aiueo";
+		String konsonan = "bcdfghjklmnpqrstvwxyz";
+		
+		
+	}
+}
