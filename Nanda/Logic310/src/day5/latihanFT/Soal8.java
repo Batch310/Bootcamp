3a3bfffeeb26c17f5293fcad920e8ca4d5503aa8
arnandasw <arnandawibawa1114@gmail.com>
2022-12-23 14:31:47 +0700

    day6

--- Nanda/Logic310/src/day5/latihanFT/Soal8.java
+++ Nanda/Logic310/src/day5/latihanFT/Soal8.java
@@ -1,5 +1,6 @@
 package day5.latihanFT;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Soal8 {
@@ -9,34 +10,39 @@ public class Soal8 {
 
 		System.out.print("Input panjang deret: ");
 		int n = input.nextInt();
-		
+
 		int[] prima = new int[n];
 		int[] fibonacci = new int[n];
 		int bilprima = 0;
 		int bilfibo = 0;
-		int current = 0; //menghitung banyak bilangan prima yang sudah ketemu
-	
-				
+		int current = 0; // menghitung banyak bilangan prima yang sudah ketemu
+		int current1 = 0;
+
 		for (int i = 1; current < n; i++) {
 			bilprima = 0;
 			for (int j = 1; j <= i; j++) {
-				if ( i%j == 0) {
-					bilprima++;
+				if (i % j == 0) {
+				bilprima++;
 				}
-			}if (bilprima == 2) {
-				System.out.print(i+ " ");
+			}
+			if (bilprima == 2) {
+				prima[current] = i;
+				System.out.print(i + " ");
 				current++;
-				
-		}
-	}
-//		System.out.println();
-//		
-//		for (int i = 1; i < fibonacci.length; i++) {
-//			System.out.print(i+ " ");
-//			bilfibo = i + current ;
-//			current = i;
-//			current = bilfibo;
-				
-				
 			}
 		}
+		System.out.println();
+		System.out.print("Bilangan Prima : ");
+		for (int i = 0; i < prima.length; i++) {
+			System.out.print(prima[i]+ " ");
+		}
+	}
+
+//		System.out.println();
+//		
+//		for (int i = 1; i <= fibonacci.length; i++) {
+//			System.out.print(i+ " ");
+//			bilfibo = i + current ;
+//			current1 = i;
+//			current1 = bilfibo;
+}
