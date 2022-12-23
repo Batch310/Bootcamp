d67c78d57ee008113be122d3495bd57f4c0095a8
masmahen <mahendradaffareza@gmail.com>
2022-12-23 09:44:24 +0700

    Summary (required)

---
+++ Daffa/Logic310Daffa/src/day5/pagi/Soal4HitungLiterBensin.java
@@ -0,0 +1,18 @@
+package day5.pagi;
+
+import java.util.Scanner;
+
+public class Soal4HitungLiterBensin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tokoKeT1 = 2000;
+		int T1keT2 = 500;
+		int T2keT3 = 1500;
+		int T3keT4 = 2500;
+		int bensin = 0;
+		
+		
+	}
+
+}
---
+++ Daffa/Logic310Daffa/src/day5/pagi/Soal5PorsiMakan.java
@@ -0,0 +1,66 @@
+package day5.pagi;
+
+import java.util.Scanner;
+
+public class Soal5PorsiMakan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		int dLaki = 2;
+		int dPerempuan = 1;
+		int remaja = 1;
+		double anak = 0.5;
+		int balita = 1;
+
+		int porsiLaki = 0;
+		int porsiPerempuan = 0;
+		int porsiRemaja = 0;
+		double porsiAnak = 0;
+		int porsiBalita = 0;
+
+		int totalOrang = 0;
+		double totalPorsi = 0;
+
+		System.out.print("Berapa laki-laki dewasa? ");
+		int LD = input.nextInt();
+		System.out.print("Berapa perempuan dewasa? ");
+		int PD = input.nextInt();
+		System.out.print("Berapa remaja? ");
+		int RE = input.nextInt();
+		System.out.print("Berapa anak? ");
+		int AN = input.nextInt();
+		System.out.print("Berapa balita? ");
+		int BA = input.nextInt();
+		
+		input.close();
+		
+		if (LD > 0) {
+			porsiLaki = LD * dLaki;
+		}
+		if (PD > 0) {
+			porsiPerempuan = PD * dPerempuan;
+		}
+		if (RE > 0) {
+			porsiRemaja = RE * remaja;
+		}
+		if (AN > 0) {
+			porsiAnak = AN * anak;
+		}
+		if (BA > 0) {
+			porsiBalita = BA * balita;
+		}
+
+		totalPorsi = porsiLaki + porsiPerempuan + porsiRemaja + porsiAnak + porsiBalita;
+		totalOrang = LD + PD + RE + AN + BA;
+
+		if (totalOrang % 2 == 1 && totalOrang > 5) {
+			totalPorsi++;
+			System.out.println("Total porsi makan adalah " + (int) totalPorsi + " porsi");
+		} else {
+			System.out.println("Total porsi makan adalah " + (int) totalPorsi + " porsi");
+		}
+	}
+
+}
