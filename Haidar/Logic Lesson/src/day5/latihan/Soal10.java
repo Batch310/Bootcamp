7a780f2381c087afca7fe9eff06f95a6764c9971
hamirf <haidar300400@gmail.com>
2022-12-24 17:16:58 +0700

    Day 5 Soal 5
    Revisi dikit

--- Haidar/Logic Lesson/src/day5/latihan/Soal10.java
+++ Haidar/Logic Lesson/src/day5/latihan/Soal10.java
@@ -31,7 +31,8 @@ public class Soal10 {
 //		int harga = 0;
 		int totalHarga = 0;
 		
-//		if (jumlahKopi >  minKopi) {
+//		if (jumlahKopi > minKopi) {
+		if (saldo >=  40000) {
 			hargaDiskon = hargaKopi*diskon/100;
 			hargaKopi -= hargaDiskon; //Harga kopi setelah diskon
 			jumlahKopi = saldo/hargaKopi;			
@@ -63,10 +64,10 @@ public class Soal10 {
 			}
 			saldoAkhir = saldo-totalHarga+totalCashback;
 //			System.out.println(hargaKopi);
-//		} else {
-//			totalHarga = jumlahKopi*hargaKopi;
-//			saldoAkhir = saldo-totalHarga;
-//		}
+		} else {
+			totalHarga = jumlahKopi*hargaKopi;
+			saldoAkhir = saldo-totalHarga;
+		}
 //		System.out.println(hargaDiskon);
 //		System.out.println(hargaKopi);
 //		System.out.println(minKopi);
