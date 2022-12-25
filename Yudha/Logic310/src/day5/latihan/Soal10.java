6ff3fd33fe29d8b1421b8dc4d8890d6e30a5cb6f
yykw <ykwyudha@gmail.com>
2022-12-26 06:41:36 +0700

    Yudha PR

--- Yudha/Logic310/src/day5/latihan/Soal10.java
+++ Yudha/Logic310/src/day5/latihan/Soal10.java
@@ -6,31 +6,78 @@ public class Soal10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//
 		Scanner input = new Scanner(System.in);
 		System.out.print("Inputkan Saldo : Rp.");
 		int saldo = input.nextInt();
 		int kopi = 18000,diskon = 0;
 		int jumlahKopi = 0,totalDiskon=0,totalHarga = 0;
-		int maxCB = 100000;
-		int maxSaldo = 100/50 *maxCB; 
-		System.out.println(kopi);
+		int maxCB = 30000;
+		int sisaDiskon=0;
+		int maxDiskon = 100000;
+		int limitJumlahDiskonKopi = 0;
+		int kopiDiskon = 0;
+		int cashBack = 0;
 		if(saldo>=40000) {
 			diskon = kopi*50/100;
-			kopi -=diskon;
+			kopiDiskon = kopi - diskon;
 			jumlahKopi = saldo / diskon;
 			totalDiskon = jumlahKopi * diskon;
-			if(totalDiskon>100000) {
-				totalDiskon = 100000;
+			totalHarga = kopiDiskon*jumlahKopi;
+			saldo = saldo - totalHarga;
+			if(totalDiskon>maxDiskon) {
+				jumlahKopi=1;
+				totalDiskon=0;
+				saldo = saldo + totalHarga;
+				while(totalDiskon<=maxDiskon) {
+					totalDiskon = diskon*jumlahKopi;
+					jumlahKopi++;
+//					System.out.println(jumlahKopi);
+				}
+				jumlahKopi = jumlahKopi-2;
+				totalDiskon = diskon*jumlahKopi;
+				sisaDiskon = maxDiskon - totalDiskon;
+				totalHarga = kopiDiskon * jumlahKopi;
+				saldo = saldo-totalHarga;
+				if(saldo >= (kopi - sisaDiskon)) {
+					saldo = saldo - (kopi-sisaDiskon);
+					jumlahKopi++;
+					totalHarga = totalHarga + (kopi-sisaDiskon);
+					totalDiskon = totalDiskon + sisaDiskon;
+					
+				}
+				if(saldo >= kopi) {
+					while(saldo>kopi) {
+						saldo = saldo - kopi;
+						jumlahKopi++;
+						totalHarga = totalHarga + kopi;
+					}
+					
+				}
 			}
-			totalHarga = kopi * jumlahKopi;
-			
-			System.out.println(diskon);
-			System.out.println(kopi);
-			System.out.println(jumlahKopi);
-			System.out.println(totalDiskon);
-			System.out.println(totalHarga);
-			
 		}
+		else {
+			jumlahKopi = saldo/kopi;
+			totalHarga = jumlahKopi* kopi;
+			saldo = saldo - totalHarga;
+		}
+		if(totalHarga/10>maxCB) {
+			cashBack = maxCB;
+		}
+		else {
+			cashBack = totalHarga/10;
+		}
+		saldo = saldo + cashBack;
+		
+		System.out.println("Jumlah cup = " + jumlahKopi + "; Saldo akhir = Rp." + saldo); 
+//		System.out.println(diskon);
+//		System.out.println(kopi);
+//		System.out.println(jumlahKopi);
+//		System.out.println(totalDiskon);
+//		System.out.println(totalHarga);
+//		System.out.println(saldo);
+//		System.out.println(kopi-sisaDiskon);
+		
 	}
 
 }
