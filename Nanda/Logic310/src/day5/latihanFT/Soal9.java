17d3a6cacc84ffecbcebcbd0d75e9025c7205687
arnandasw <arnandawibawa1114@gmail.com>
2022-12-26 15:33:26 +0700

    day7

--- Nanda/Logic310/src/day5/latihanFT/Soal9.java
+++ Nanda/Logic310/src/day5/latihanFT/Soal9.java
@@ -1,10 +1,32 @@
 package day5.latihanFT;
+import java.util.Scanner;
 
 public class Soal9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Masukkan jalur Hattori: ");
+		String JalurHattori = input.nextLine();
+		
+		int gunung = 0;
+		int lembah = 0;
+		int jalur = 0;
+		
+		String[] arrInput = JalurHattori.split(" ");
+		for (int i = 0; i < arrInput.length; i++) {
+			if (arrInput[i].equals("N")) {
+				jalur++;
+			} else if (arrInput[i].equals("T")) {
+				jalur--;
+			}
+			if ((jalur == 0) && (arrInput[i].equals("T"))) {
+				gunung++;
+			} else if ((jalur == 0) && (arrInput[i].equals("N"))) {
+				lembah++;
+			}
+		}
+		System.out.println("Gunung yang dilewati Hattori = " + gunung + " Gunung");
+		System.out.println("Lembah yang dilewati Hattori = " + lembah + " Lembah");
 	}
-
 }
