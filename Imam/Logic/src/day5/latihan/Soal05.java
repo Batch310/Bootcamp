d5788ef6e1b8abf957a9bcd3c021b9e095eced7b
Imam Aditya <120548261+ImamAditya@users.noreply.github.com>
2022-12-26 08:12:12 +0700

    Latihan Exam

--- Imam/Logic/src/day5/latihan/Soal05.java
+++ Imam/Logic/src/day5/latihan/Soal05.java
@@ -11,73 +11,46 @@ public class Soal05 {
 //		// R 2 orang remaja =  2 mie ayam
 //        // A 1 anak = 1/2 nasgor
 //		// B 1 balita = 1 mangkok kecil bubur
-		
-//		jika jumlah orang ganjil P += piring nasgor
-		
-		Scanner scanner = new Scanner(System.in);
-		
-	   int porsi = 0;
-	   int jumlahPorsi;
-       int orang = 0;
-				
-			System.out.print("Laki-laki dewasa = ");
-             int l =scanner.nextInt();
-                orang+=l;
-//				porsi=l*2;
-			
-			System.out.print("Perempuan dewasa = ");
-			int p=scanner.nextInt();
-			  orang+=p;
-//			porsi+=p;
-			
-			System.out.print("Remaja = ");
-			int r=scanner.nextInt();
-			  orang+=r;
-//			porsi+=r;
-			
-			System.out.print("anak = ");
-			double a = scanner.nextDouble();
-			  orang+=a;
-//			if(a==0) {
-//				porsi+=a;
-//			}else {
-//			    porsi+=a/2 ;
-//			}
-			
-			System.out.print("Balita = ");
-			int b = scanner.nextInt();
-			  orang+=b;
-//			porsi+=b;
-			
-		System.out.print("Jumlah orang ="+orang);
-		System.exit(0);
-		
-		
-		if(orang >= 5 && orang % 2!=0) {
-			 porsi+=1;
-		System.out.print("Jumlah porsi = " + porsi);	     
-		}
-		else {
-			System.out.print("Jumlah porsi = "+ porsi );		
-		} 
-		
-		System.exit(b);
-		
-		
-		 
-//		if(l>=0) {
-//			jumlahPorsi=l*2;
-//		}
-//		else if(p>=0) {
-//			jumlahPorsi=p;
-//		}else if(r>=0) {
-//			jumlahPorsi=r;
-//		}else if(a>=0) {
-//			jumlahPorsi=a;
-//		}else if(b>=0) {
-//			jumlahPorsi=b/2;
-//		}
-		
-	}
 
+//		jika jumlah orang ganjil, 1 P += piring nasgor
+
+//laki-laki dewasa = 3 orang; perempuan = 1 orang;Balita = 1 orang; laki-laki dewasa 1 orang
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Masukkan pelanggan = ");
+		String[] inputan = scanner.nextLine().split(";"); //terakhir pake ";" >> .length
+
+		int sumJmlOrang = 0;
+		int jmlPerempuan = 0;
+		double porsi = 0;
+		double sumPorsi = 0;
+
+		for (int i = 0; i < inputan.length; i++) {
+
+			String[] pelanggan = inputan[i].split("= ");
+			String orang = pelanggan[0]; // cari orang
+			int jumlahOrang = Integer.parseInt(pelanggan[1].substring(0, 1));//Ubah string ke int(jmlOrang)
+			String totaljmlOrang = pelanggan[1].substring(0, 1);
+			if (orang.equalsIgnoreCase("laki-laki dewasa ") || orang.equalsIgnoreCase("remaja")) {
+				porsi = jumlahOrang * 2;
+			} else if (orang.equalsIgnoreCase("perempuan dewasa ")) {
+				porsi = jumlahOrang;
+				jmlPerempuan += jumlahOrang;
+			} else if (orang.equalsIgnoreCase("anak ")) {
+				porsi = jumlahOrang*0.5;
+			} else if (orang.equalsIgnoreCase("balita ")) {
+				porsi = jumlahOrang;
+			}
+			sumJmlOrang += jumlahOrang;
+			sumPorsi += porsi;
+
+		}
+
+		if (sumJmlOrang > 5 && sumJmlOrang % 2 != 0) {
+
+			sumPorsi += jmlPerempuan;
+		}
+		System.out.print(sumPorsi + " porsi");
+
+	}
 }
