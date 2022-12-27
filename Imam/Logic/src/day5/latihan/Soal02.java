1602b0536e65a5978cc2d3cc45ca04709c581452
Imam Aditya <120548261+ImamAditya@users.noreply.github.com>
2022-12-27 08:02:18 +0700

    LatihanExam

--- Imam/Logic/src/day5/latihan/Soal02.java
+++ Imam/Logic/src/day5/latihan/Soal02.java
@@ -1,5 +1,6 @@
 package day5.latihan;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,40 +15,41 @@ public class Soal02 {
 		String[] arrKalimat = kalimat.split("");
 		Arrays.sort(arrKalimat);
 
-//		for (String string : arrKalimat) {
-//		System.out.print(string);
-//		}
-//		
-//		System.exit(0);
+		                                        // tipe double, menghapus duplicate data/mengembalikan nilai beda
+		arrKalimat = Arrays.stream(arrKalimat).distinct().toArray(String[]::new);
+		                                                  // convert array ori
 
 		String voc = "aiueo";
 		String kon = "qwrtypsdfghjklzxcvbnm";
 		String hurufVoc = "";
 		String hurufKon = "";
+
 		for (int i = 0; i < arrKalimat.length; i++) {
 			if (voc.contains(arrKalimat[i])) {
 				hurufVoc = hurufVoc + arrKalimat[i];
 			} else if (kon.contains(arrKalimat[i])) {
 				hurufKon = hurufKon + arrKalimat[i];
 			}
-
 		}
+//		
+//		String[] newVoc = hurufVoc.split("");
+//		
+//		int found=0;
+//		String[] NewVoc= new String[newVoc.length];
+//		for (int j = 1; j < newVoc.length; j++) {
+//			
+//			if (newVoc[j].equals(newVoc[j-1])) {
+//				NewVoc[found] = newVoc[j];
+//				found++;
+//			} else {
+//				NewVoc[found] = newVoc[j];
+//			}
+//		}
+		
 
 		System.out.print("Huruf vocal = " + hurufVoc);
 		System.out.println();
 		System.out.print("Huruf konsonan = " + hurufKon);
-
-//		String[] arrVocal = new String[] { "a", "e", "i", "o", "u" };
-
-//		String[] voc= new String[];
-//		String[] kon=new String[];
-//		for (int i = 0; i < arrVocal.length; i++) {
-//		        if(n.contains(arrVocal[i])) {
-//		        	voc=arrVocal[i];
-//		        }else if(n.contains(arrVocal[1])){
-//		        	
-//		        }
-//			
 
 	}
 
