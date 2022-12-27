2cdaa856f1e45e02707c0b2b8272abf5120e9747
Yudha Yuniartha Kusuma Wardhana <ykwyudha@gmail.com>
2022-12-27 16:54:12 +0700

    Yudha 27 Des

--- Yudha/Logic310/src/day5/latihan/Soal2.java
+++ Yudha/Logic310/src/day5/latihan/Soal2.java
@@ -9,24 +9,28 @@ public class Soal2 {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.println("Masukkan Kalimat : ");
-		String kalimat = input.nextLine().toLowerCase();//kalimat berupa input yang telah diubah huruf kecil
-		String [] arrayKalimat = kalimat.split(""); // yykw => y,y,k,w
+		String kalimat = input.nextLine().toLowerCase();// kalimat berupa input yang telah diubah huruf kecil
+		String[] arrayKalimat = kalimat.split(""); // yykw => y,y,k,w
 		Arrays.sort(arrayKalimat);// y, y, k, w => k, w, y, y
-		String vokal =  "aeiou";
+		String vokal = "aeiou";
 		String konsonan = "bcdfghjklmnpqrstvwxyz";
-		String vokalKalimat="",konsonanKalimat="";
-		for (int i = 0; i < arrayKalimat.length; i++) {// perulangan untuk setiap 
-			//System.out.println(kalimat.charAt(i));
-			if(vokal.contains(arrayKalimat[i])) {
-				vokalKalimat+=arrayKalimat[i];
+		String vokalKalimat = "", konsonanKalimat = "";
+		for (int i = 0; i < arrayKalimat.length; i++) {// perulangan untuk setiap
+			// System.out.println(kalimat.charAt(i));
+			if (vokal.contains(arrayKalimat[i])) {
+				if (!(vokalKalimat.contains(arrayKalimat[i]))) {
+					vokalKalimat += arrayKalimat[i];
+				} 
 			}
-			if(konsonan.contains(arrayKalimat[i])) {
-				konsonanKalimat+=arrayKalimat[i];
+			if ((konsonan.contains(arrayKalimat[i]))) {
+				if (!(konsonanKalimat.contains(arrayKalimat[i]))) {
+					konsonanKalimat += arrayKalimat[i];
+				}
 			}
-			
+
 		}
 		System.out.println("Huruf Vokal : " + vokalKalimat);
 		System.out.println("Huruf Konsonan : " + konsonanKalimat);
-		}//challege : cetak vokal dan konsonan unik
+	}// challege : cetak vokal dan konsonan unik
 
 }
