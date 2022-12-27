cce854df330188335aea03fae1291ab77cf033dc
masmahen <mahendradaffareza@gmail.com>
2022-12-27 13:04:55 +0700

    1

--- Daffa/Logic310Daffa/src/day5/pagi/Soal7Kartu.java
+++ Daffa/Logic310Daffa/src/day5/pagi/Soal7Kartu.java
@@ -14,20 +14,25 @@ public class Soal7Kartu {
 		int tawaran = 0;
 		int kotakA = 0;
 		int kotakB = 0;
+		int winner = 0;
 		String pilihan1 = "A";
 		String pilihan2 = "B";
 		String pilihan3 = "Menyerah";
 		System.out.println("Masukkan jumlah kartu : ");
 		kartuAnda = input.nextInt();
-		kartuKomputer = input.nextInt();
-		System.out.println("Masukkan jumlah tawaran : ");
-		tawaran = input.nextInt();
-
-		kotakA = acak.nextInt(10);
-		kotakB = acak.nextInt(10);
-
-		if (tawaran <= kartuAnda && tawaran <= kartuKomputer) {
-			while (kartuAnda > 0 && kartuKomputer > 0) {
+		kartuKomputer = kartuAnda;
+		System.out.println("Kartu Anda : " + kartuAnda);
+		System.out.println("Kartu Komputer : " + kartuKomputer);
+		while (kartuAnda > 0 && kartuKomputer > 0) {
+			kotakA = acak.nextInt(10);
+			kotakB = acak.nextInt(10);
+//			System.out.println(kotakA);
+//			System.out.println(kotakB);
+			System.out.println("Masukkan jumlah tawaran : ");
+			tawaran = input.nextInt();
+			System.out.println("Kartu Anda : " + kartuAnda);
+			System.out.println("Kartu Komputer : " + kartuKomputer);
+			if (tawaran <= kartuAnda && tawaran <= kartuKomputer) {
 				System.out.println("Pilih kotak A atau B atau Menyerah? ");
 				String choose = input.next();
 				if (choose.equalsIgnoreCase(pilihan1)) {
@@ -35,42 +40,78 @@ public class Soal7Kartu {
 						kartuAnda -= tawaran;
 						kartuKomputer += tawaran;
 						System.out.println("Tebakan salah, kartu berkurang sebanyak " + tawaran);
+						System.out.println("Kotak A : " + kotakA);
+						System.out.println("Kotak B : " + kotakB);
 					} else if (kotakA > kotakB) {
 						kartuAnda += tawaran;
 						kartuKomputer -= tawaran;
 						System.out.println(kotakA + " Tebakan anda benar");
+						System.out.println("Kotak A : " + kotakA);
+						System.out.println("Kotak B : " + kotakB);
 					} else {
-						kartuAnda += 0;
-						kartuKomputer += 0;
+						winner = acak.nextInt(2);
+						if (winner == 1) {
+							System.out.println("Anda Hoki");
+							kartuAnda += tawaran;
+							kartuKomputer -= tawaran;
+							System.out.println("Kotak A : " + kotakA);
+							System.out.println("Kotak B : " + kotakB);
+						} else {
+							System.out.println("Anda nggak hoki");
+							kartuAnda -= tawaran;
+							kartuKomputer += tawaran;
+							System.out.println("Kotak A : " + kotakA);
+							System.out.println("Kotak B : " + kotakB);
+						}
 					}
 				} else if (choose.equalsIgnoreCase(pilihan2)) {
 					if (kotakA > kotakB) {
 						kartuAnda -= tawaran;
 						kartuKomputer += tawaran;
 						System.out.println("Tebakan salah, kartu berkurang sebanyak " + tawaran);
+						System.out.println("Kotak A : " + kotakA);
+						System.out.println("Kotak B : " + kotakB);
 					} else if (kotakA < kotakB) {
 						kartuAnda += tawaran;
 						kartuKomputer -= tawaran;
 						System.out.println(kotakB + " Tebakan anda benar");
+						System.out.println("Kotak A : " + kotakA);
+						System.out.println("Kotak B : " + kotakB);
 					} else {
-						kartuAnda += 0;
-						kartuKomputer += 0;
+						winner = acak.nextInt(2);
+						if (winner == 1) {
+							System.out.println("Anda Hoki");
+							kartuAnda += tawaran;
+							kartuKomputer -= tawaran;
+							System.out.println("Kotak A : " + kotakA);
+							System.out.println("Kotak B : " + kotakB);
+						} else {
+							System.out.println("Anda nggak hoki");
+							kartuAnda -= tawaran;
+							kartuKomputer += tawaran;
+							System.out.println("Kotak A : " + kotakA);
+							System.out.println("Kotak B : " + kotakB);
+						}
 					}
-				} else if(choose.equalsIgnoreCase(pilihan3)) {
+				} else if (choose.equalsIgnoreCase(pilihan3)) {
 					System.out.println("You Surrendered, You Lose");
 					System.exit(0);
 				} else {
 					System.out.println("Pilihan tidak dikenal");
-					System.exit(0);
 				}
+			} else if (tawaran >= kartuAnda || tawaran >= kartuKomputer) {
+				System.out.println("Tawaran tidak boleh lebih dari jumlah kartu!");
 			}
-			if (kartuAnda > 0) {
-				System.out.println("You Win");
-			} else if (kartuKomputer > 0) {
-				System.out.println("You Lose");
-			}
-		} else {
-			System.out.println("Tawaran tidak boleh lebih dari jumlah kartu!");
+
+		}
+		if (kartuAnda > 0) {
+			System.out.println("You Win");
+			System.out.println("Kotak A : " + kotakA);
+			System.out.println("Kotak B : " + kotakB);
+		} else if (kartuKomputer > 0) {
+			System.out.println("You Lose");
+			System.out.println("Kotak A : " + kotakA);
+			System.out.println("Kotak B : " + kotakB);
 		}
 		input.close();
 	}
