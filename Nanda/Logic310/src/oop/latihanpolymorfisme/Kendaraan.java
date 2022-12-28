af1a88c8a99d33594d3dd4c89dfac999bbb31f0f
arnandasw <arnandawibawa1114@gmail.com>
2022-12-28 16:12:27 +0700

    day9

--- Nanda/Logic310/src/oop/latihanpolymorfisme/Kendaraan.java
+++ Nanda/Logic310/src/oop/latihanpolymorfisme/Kendaraan.java
@@ -1,4 +1,4 @@
-package oop.latihanenkapsulasi;
+package oop.latihanpolymorfisme;
 
 class Kendaraan {
 	private String modelParam;
---
+++ Nanda/Logic310/src/oop/latihanpolymorfisme/Mobil.java
@@ -0,0 +1,17 @@
+package oop.latihanpolymorfisme;
+
+public class Mobil extends Kendaraan{
+	
+	public void bukaPintu() {
+		System.out.println("Pintu Terbuka buat BosQ");
+	}
+	
+	public void isiBensin() {
+		System.out.println("Isi Bensin sebanyak-banyaknya ahh~");
+	}
+	
+	public void isiBensin(int liter) {
+		System.out.println("IsiBensin sebanyak " + liter + " liter");
+	}
+	
+}
