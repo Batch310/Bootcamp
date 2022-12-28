4e125815cb94664486446280694fac7a5e01ebf9
masmahen <mahendradaffareza@gmail.com>
2022-12-28 13:39:08 +0700

    1

---
+++ Daffa/Logic310Daffa/src/day9/pagi/Car.java
@@ -0,0 +1,19 @@
+package day9.pagi;
+
+public class Car extends Vehicle {
+
+	String kemudi;
+
+	public Car() {
+		super();
+	}
+
+	public Car(String nama) {
+		super(nama);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void bukaJendela() {
+		System.out.println("Buka Jendela");
+	}
+}
---
+++ Daffa/Logic310Daffa/src/day9/pagi/RunCar.java
@@ -0,0 +1,13 @@
+package day9.pagi;
+
+public class RunCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicle abc = new Vehicle("Sedan");
+
+		abc.belokKiri();
+		abc.belokKanan();
+	}
+
+}
