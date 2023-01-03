39eca7042d33cf9438851956521262d39b703f5c
masmahen <mahendradaffareza@gmail.com>
2023-01-03 13:48:08 +0700

    1

--- Daffa/Logic310Daffa/src/simulasiFT01/SimFT10.java
+++ Daffa/Logic310Daffa/src/simulasiFT01/SimFT10.java
@@ -11,12 +11,18 @@ public class SimFT10 {
 		String alphabet = scanner.next();
 		System.out.println("Masukkan input angka : ");
 		String angka = scanner.next();
-
+		scanner.close();
 		String[] arrAlphabet = alphabet.split("");
 		String[] arrAngka = angka.split(",");
 
 		if (arrAlphabet.length == arrAngka.length) {
-
+			for (int i = 0; i < arrAlphabet.length; i++) {
+				for (int j = 0; j < arrAngka.length; j++) {
+					if(true) {
+						
+					}
+				}
+			}
 		} else {
 			System.out.println("Panjang deret harus sama");
 		}
