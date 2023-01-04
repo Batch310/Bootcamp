c75ffdae4b7b2d991bc48767c9032eba857c3c19
Tyannn <tyanprisma00@gmail.com>
2023-01-04 16:53:29 +0700

    ..

--- Tyannn/Logic310/src/simulasift1/soal3.java
+++ Tyannn/Logic310/src/simulasift1/soal3.java
@@ -8,29 +8,28 @@ public class soal3 {
 		Scanner input = new Scanner(System.in);
 		// x=5 5,10,15 ....
 		// y=8 5,16,24 ....
-		// n = 5						n=3
-		// outuput 40 80 120 160		40 80 120
-		
-		
-		int n = 5;
+		// n = 5 n=3
+		// outuput 40 80 120 160 40 80 120
 
-		int x = 5;
-		int y = 8;
+		System.out.print("Input n = ");
+		int n = input.nextInt();
+
+		int[] arr = new int[n];
+
 		
-		for(int i=1; i<=n; i++) {
-			x = x * 2;
-			System.out.print(x+" ");
+		int nilaiAwal = 5;
+		for (int index = 0; index < arr.length; nilaiAwal+=5) {
+			if (nilaiAwal % 5 == 0 && nilaiAwal % 8 == 0) {
+				arr[index] = nilaiAwal;
+				index++;
+	
+			}
 		}
-		
-		System.out.println();
-		for(int i=1; i<=n; i++) {
-			y = y * 2;
-			System.out.print(y+" ");
+
+		for (int i : arr) {
+			System.out.println(i+" ");
 		}
-		
-		if(x==y) {
-			
-		}
+
 	}
 
 }
