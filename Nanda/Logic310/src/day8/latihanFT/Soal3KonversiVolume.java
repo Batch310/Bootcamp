86fec695bf68b9a1e7a2d0d59b1b32798db46d70
arnandasw <arnandawibawa1114@gmail.com>
2023-01-05 16:37:13 +0700

    day17

--- Nanda/Logic310/src/day8/latihanFT/Soal3KonversiVolume.java
+++ Nanda/Logic310/src/day8/latihanFT/Soal3KonversiVolume.java
@@ -5,9 +5,44 @@ import java.util.Scanner;
 public class Soal3KonversiVolume {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		
+		Scanner masukan = new Scanner(System.in);
+
+		System.out.print("1 ");
+		String barang1 = masukan.next();
+
+		System.out.print("... ");
+		String barang2 = masukan.next();
+
+		double volume = 0;
+
+		// Karena ada 4 barang, maka setiap 1 barang dibandingkan sama 3 barang lainnya
+		if (barang1.equalsIgnoreCase("botol") && barang2.equalsIgnoreCase("gelas")) {
+			volume = 2;
+		} else if (barang1.equalsIgnoreCase("botol") && barang2.equalsIgnoreCase("cangkir")) {
+			volume = 5;
+		} else if (barang1.equalsIgnoreCase("botol") && barang2.equalsIgnoreCase("teko")) {
+			volume = 0.2;
+		} else if (barang1.equalsIgnoreCase("teko") && barang2.equalsIgnoreCase("cangkir")) {
+			volume = 25;
+		} else if (barang1.equalsIgnoreCase("teko") && barang2.equalsIgnoreCase("gelas")) {
+			volume = 10;
+		} else if (barang1.equalsIgnoreCase("teko") && barang2.equalsIgnoreCase("botol")) {
+			volume = 5;
+		} else if (barang1.equalsIgnoreCase("gelas") && barang2.equalsIgnoreCase("cangkir")) {
+			volume = 2.5;
+		} else if (barang1.equalsIgnoreCase("gelas") && barang2.equalsIgnoreCase("teko")) {
+			volume = 0.1;
+		} else if (barang1.equalsIgnoreCase("gelas") && barang2.equalsIgnoreCase("botol")) {
+			volume = 0.5;
+		} else if (barang1.equalsIgnoreCase("cangkir") && barang2.equalsIgnoreCase("teko")) {
+			volume = 0.04;
+		} else if (barang1.equalsIgnoreCase("cangkir") && barang2.equalsIgnoreCase("botol")) {
+			volume = 0.2;
+		} else if (barang1.equalsIgnoreCase("cangkir") && barang2.equalsIgnoreCase("gelas")) {
+			volume = 0.4;
+		}
+		System.out.println("1 " + barang1 + " = " + volume + " " + barang2);
+		masukan.close();
 
 	}
 
