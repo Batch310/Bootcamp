bfbf10a50b90cf7c0ec621d24ecd9add599ded96
hamirf <haidar300400@gmail.com>
2023-01-05 10:14:29 +0700

    Coba OOP Protected Modifier

---
+++ Haidar/Logic Lesson/src/coba/oop/Person.java
@@ -0,0 +1,8 @@
+package coba.oop;
+
+public class Person {
+	protected String fname = "John";
+	protected String lname = "Doe";
+	protected String email = "john@doe.com";
+	protected int age = 24;
+}
---
+++ Haidar/Logic Lesson/src/coba/oop/Student.java
@@ -0,0 +1,12 @@
+package coba.oop;
+
+public class Student {
+	private int graduationYear = 2018;
+
+	public static void main(String[] args) {
+		Person myObj = new Person();
+		System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+		System.out.println("Email: " + myObj.email);
+		System.out.println("Age: " + myObj.age);
+	}
+}
