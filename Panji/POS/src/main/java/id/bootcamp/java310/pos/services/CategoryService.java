da4dfb2e3926dda08822b1c84781ae8ed5d2b650
Panji Tektok <panjibernadinus@gmail.com>
2023-01-12 17:05:47 +0700

    tek3

--- Panji/POS/src/main/java/id/bootcamp/java310/pos/services/CategoryService.java
+++ Panji/POS/src/main/java/id/bootcamp/java310/pos/services/CategoryService.java
@@ -45,36 +45,56 @@ public class CategoryService {
 		}
 		return catList;
 	}
-	//cara 3
-	public List<CategoryDTO> getAll3(){
-		List<Tuple> listSumber = cr.getAll3();
-		
-		//List kosong
-		List<CategoryDTO> listHasil = new ArrayList<>();
-		
-		for (int i = 0; i < listSumber.size(); i++) {
-			CategoryDTO cat = new CategoryDTO();
-			cat.setId(((BigInteger)listSumber.get(i).get("id")).longValue());
-			cat.setInitial((String)listSumber.get(i).get("initial"));
-			cat.setName((String)listSumber.get(i).get("name"));
-			cat.setActive((boolean)listSumber.get(i).get("active"));
-			
-			listHasil.add(cat);
-		}
-		return listHasil;
-	}
+	// cara 3
+//	public List<CategoryDTO> getAll3(){
+//		List<Tuple> listSumber = cr.getAll3();
+//		
+//		//List kosong
+//		List<CategoryDTO> listHasil = new ArrayList<>();
+//		
+//		for (int i = 0; i < listSumber.size(); i++) {
+//			CategoryDTO cat = new CategoryDTO();
+//			cat.setId(((BigInteger)listSumber.get(i).get("id")).longValue());
+//			cat.setInitial((String)listSumber.get(i).get("initial"));
+//			cat.setName((String)listSumber.get(i).get("name"));
+//			cat.setActive((boolean)listSumber.get(i).get("active"));
+//			
+//			listHasil.add(cat);
+//		}
+//		return listHasil;
+//	}
 	// Cara 4
-	public List<CategoryDTO> getAll4(){
-		return cr.getAll4();
-	}
-	// Cara 5
-	public List<CategoryDTO> getAll5(){
-		return cr.getAll5();
-	}
+//	public List<CategoryDTO> getAll4(){
+//		return cr.getAll4();
+//	}
+//	// Cara 5
+//	public List<CategoryDTO> getAll5(){
+//		return cr.getAll5();
+//	}
 
 	// Create
 	// Cara 1 - memakai fungsi bawaan JPARepository
-	public Long insert1(CategoryDTO dto) {
+	public Long insert1(CategoryDTO dto) throws Exception {
+		// Validasi
+
+		boolean isInitialExists = cr.isInitialExists(dto.getInitial());
+		if (isInitialExists == true) {
+			throw new Exception("11-Initial sudah terpakai");
+		}
+		boolean isNameExists = cr.isNameExists(dto.getName());
+		if (isNameExists == true) {
+			throw new Exception("12-Name sudah terpakai");
+		}
+		if (dto.getInitial().length() > 10) {
+			throw new Exception("13-Initial tidak boleh lebih dari 10 karakter");
+		}
+		if (dto.getName().length() > 50) {
+			throw new Exception("14-Nama tidak boleh lebih dari 50 karakter");
+		}
+		if (dto.getCreate_by().length() > 50) {
+			throw new Exception("17-Create By tidak boleh lebih dari 50 karakter");
+		}
+
 		// Mapping DTO ke Entity
 		CategoryEntity hasil = new CategoryEntity();
 		hasil.setInitial(dto.getInitial());
@@ -82,26 +102,50 @@ public class CategoryService {
 		hasil.setActive(dto.isActive());
 		hasil.setCreateBy(dto.getCreate_by());
 		hasil.setCreateDate(new Date());
-		
-		//Insert Entity
+
+		// Insert Entity
 		CategoryEntity done = cr.save(hasil);
-		return done.getId();
+		return done.getId(); //
 	}
 	// Cara 2
-	public Long insert2(CategoryDTO dto) {
-		return cr.insert2(dto, new Date());
-	}
+//	public Long insert2(CategoryDTO dto) {
+//		return cr.insert2(dto, new Date());
+//	}
 
 	// Update
-	public void update(CategoryDTO dto) {
+	public void update(CategoryDTO dto) throws Exception {
+		// Validasi
+
+		boolean isInitialExists = cr.isInitialExists(dto.getInitial());
+		if (isInitialExists == true) {
+			throw new Exception("11-Initial sudah terpakai");
+		}
+		boolean isNameExists = cr.isNameExists(dto.getName());
+		if (isNameExists == true) {
+			throw new Exception("12-Name sudah terpakai");
+		}
+		if (dto.getInitial().length() > 10) {
+			throw new Exception("13-Initial tidak boleh lebih dari 10 karakter");
+		}
+		if (dto.getName().length() > 50) {
+			throw new Exception("14-Nama tidak boleh lebih dari 50 karakter");
+		}
+		if (dto.getModify_by().length() > 50) {
+			throw new Exception("16-Modify By tidak boleh lebih dari 50 karakter");
+		}
+
 		cr.update(dto, new Date());
 	}
 
 	// Delete
-	public void delete(Long id) {
+	public void delete(Long id) throws Exception {
+		boolean isCategoryUsedByVariant = cr.isCategoryUsedByVariant(id);
+		if(isCategoryUsedByVariant) {
+			throw new Exception("15-Category dipakai, tidak dapat dihapus");
+		}
 		// Cara 1
-		//cr.deleteById(id);
-		
+		// cr.deleteById(id);
+
 		// Cara 2
 		cr.delete(id);
 	}
--- Panji/POS/src/main/java/id/bootcamp/java310/pos/services/VariantService.java
+++ Panji/POS/src/main/java/id/bootcamp/java310/pos/services/VariantService.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Tuple;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import id.bootcamp.java310.pos.dto.CategoryDTO;
 import id.bootcamp.java310.pos.dto.VariantDTO;
 import id.bootcamp.java310.pos.entities.VariantEntity;
 import id.bootcamp.java310.pos.repositories.VariantRepository;
@@ -16,12 +19,12 @@ public class VariantService {
 
 	@Autowired
 	private VariantRepository vr;
-	
-	public List<VariantDTO> getAllVariant(){
+
+	public List<VariantDTO> getAllVariant() {
 		List<VariantEntity> varSumber = vr.findAll();
-		
+
 		List<VariantDTO> varList = new ArrayList<>();
-		
+
 		for (int i = 0; i < varSumber.size(); i++) {
 			VariantDTO var = new VariantDTO();
 			var.setId(varSumber.get(i).getId());
@@ -30,23 +33,74 @@ public class VariantService {
 			var.setInitial(varSumber.get(i).getInitial());
 			var.setName(varSumber.get(i).getName());
 			var.setActive(varSumber.get(i).isActive());
-			
+
 			varList.add(var);
 		}
 		return varList;
 	}
+
 	// Create
-	public Long insert2(VariantDTO dto) {
+	public Long insert2(VariantDTO dto) throws Exception {
+		boolean isInitialExists = vr.isInitialExists(dto.getInitial());
+		if (isInitialExists == true) {
+			throw new Exception("11-Initial sudah terpakai");
+		}
+		boolean isNameExists = vr.isNameExists(dto.getName());
+		if (isNameExists == true) {
+			throw new Exception("12-Name sudah terpakai");
+		}
+		boolean isBentarTakCobaDulu = vr.isBentarTakCobaDulu(dto.getCategory_id());
+		if (isBentarTakCobaDulu == false) {
+			throw new Exception("18-Category tidak ada !");
+		}
+		if (dto.getInitial().length() > 10) {
+			throw new Exception("13-Initial tidak boleh lebih dari 10 karakter");
+		}
+		if (dto.getName().length() > 50) {
+			throw new Exception("14-Nama tidak boleh lebih dari 50 karakter");
+		}
+		if (dto.getCreate_by().length() > 50) {
+			throw new Exception("17-Create By tidak boleh lebih dari 50 karakter");
+		}
+
 		return vr.insert2(dto, new Date());
 	}
-	
+
 	// Update
-	public void update(VariantDTO dto) {
+	public void update(VariantDTO dto) throws Exception {
+		boolean isInitialExists = vr.isInitialExists(dto.getInitial());
+		if (isInitialExists == true) {
+			throw new Exception("11-Initial sudah terpakai");
+		}
+		boolean isNameExists = vr.isNameExists(dto.getName());
+		if (isNameExists == true) {
+			throw new Exception("12-Name sudah terpakai");
+		}
+		boolean isBentarTakCobaDulu = vr.isBentarTakCobaDulu(dto.getCategory_id());
+		if (isBentarTakCobaDulu == false) {
+			throw new Exception("18-Category tidak ada !");
+		}
+		if (dto.getInitial().length() > 10) {
+			throw new Exception("13-Initial tidak boleh lebih dari 10 karakter");
+		}
+		if (dto.getName().length() > 50) {
+			throw new Exception("14-Nama tidak boleh lebih dari 50 karakter");
+		}
+		if (dto.getModify_by().length() > 50) {
+			throw new Exception("16-Modify By tidak boleh lebih dari 50 karakter");
+		}
+
 		vr.update(dto, new Date());
 	}
-	
+
 	// Delete
-	public void delete(Long id) {
+	public void delete(Long id) throws Exception {
+		boolean isVariantUsedByProduct = vr.isVariantUsedByProduct(id);
+		if(isVariantUsedByProduct) {
+			throw new Exception("15-Variant dipakai, tidak dapat dihapus");
+		}
+		
 		vr.delete(id);
 	}
+
 }
