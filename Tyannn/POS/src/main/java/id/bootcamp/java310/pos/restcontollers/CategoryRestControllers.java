45f9b7fc4ef5544b6e169978428fef05ba66077f
Tyannn <tyanprisma00@gmail.com>
2023-01-12 17:02:36 +0700

    PR no 8

--- Tyannn/POS/src/main/java/id/bootcamp/java310/pos/restcontollers/CategoryRestControllers.java
+++ Tyannn/POS/src/main/java/id/bootcamp/java310/pos/restcontollers/CategoryRestControllers.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import id.bootcamp.java310.pos.dto.CategoryDTO;
 import id.bootcamp.java310.pos.services.CategoryService;
+import id.bootcamp.java310.pos.utils.Resp;
 
 @RestController
 @RequestMapping("/api/category") // Mapping URL secara umum
@@ -24,35 +25,122 @@ public class CategoryRestControllers {
 
 	// localhost/api/category/get
 	@GetMapping("/get")
-	public List<CategoryDTO> getAll() {
+	public Resp<List<CategoryDTO>> getAll() {
+
+		// Mengemas Response API
+		int code = 200;
+		String message = "Sukses";
+		List<CategoryDTO> data = cs.getAllCategories();
+
+		Resp<List<CategoryDTO>> response = new Resp<>();
+		response.setCode(code);
+		response.setMessage(message);
+		response.setData(data);
+
+		return response;
+
 		// cara 1,2
-		//return cs.getAllCategories();
-		
-		//cara 3
-		//return cs.getAll3();
-		
-		//cara 4, 5
-		return cs.getAll45();
+		// return cs.getAllCategories();
+
+		// cara 3
+		// return cs.getAll3();
+
+		// cara 4, 5
+		// return cs.getAll45();
 
 	}
-	
+
 	@PostMapping("/insert")
-	public Long insertCategory(@RequestBody CategoryDTO dto) {
+	public Resp<Long> insertCategory(@RequestBody CategoryDTO dto) {
+		try {
+			// Mengemas Response API
+			int code = 200;
+			String message = "Category Berhasil ditambahkan!";
+			Long data = cs.insert1(dto);
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+			response.setData(data);
+
+			return response;
+
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage();// "11-blabla"
+			String[] split = exceptionMessage.split("-");
+
+			int code = Integer.parseInt(split[0]);
+			String message = split[1];
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+			return response;
+		}
+
 		// Cara 1
-		//return cs.insert1(dto);
-		return cs.insert2(dto);
+		// return cs.insert1(dto);
+		// return cs.insert2(dto);
 	}
-	
+
 	// localhost/api/category/update
 	@PutMapping("/update")
-	public void updateCategory(@RequestBody CategoryDTO dto) {
-		cs.update(dto);
+	public Resp<Long> updateCategory(@RequestBody CategoryDTO dto) {
+		try {
+			// Mengemas Response API
+			int code = 200;
+			String message = "Category Berhasil diubah!";
+			// jangan lupa dipanggil
+			cs.update(dto);
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+
+			return response;
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage();// "11-blabla"
+			String[] split = exceptionMessage.split("-");
+
+			int code = Integer.parseInt(split[0]);
+			String message = split[1];
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+			return response;
+		}
+
+		// cs.update(dto);
 	}
-	
+
 	@DeleteMapping("/delete")
-	public void deleteCategory(@RequestParam("id") Long id) {
-		cs.delete(id);
+	public Resp<Long> deleteCategory(@RequestParam("id") Long id) {
+		try {
+			int code = 200;
+			String message = "Category Berhasil dihapus!";
+			// jangan lupa dipanggil
+			cs.delete(id);
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+
+			return response;
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage();// "11-blabla"
+			String[] split = exceptionMessage.split("-");
+
+			int code = Integer.parseInt(split[0]);
+			String message = split[1];
+
+			Resp<Long> response = new Resp<>();
+			response.setCode(code);
+			response.setMessage(message);
+			return response;
+		}
+		// Mengemas Response API
+
 	}
-	
-	
+
 }
