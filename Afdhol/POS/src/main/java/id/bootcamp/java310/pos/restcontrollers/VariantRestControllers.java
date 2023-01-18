2601d8b95d01693eda8918d9045c9bae2ff27cf4
Afdhol <afdholbasil@gmail.com>
2023-01-18 09:38:08 +0700

    njedot

--- Afdhol/POS/src/main/java/id/bootcamp/java310/pos/restcontrollers/VariantRestControllers.java
+++ Afdhol/POS/src/main/java/id/bootcamp/java310/pos/restcontrollers/VariantRestControllers.java
@@ -169,9 +169,23 @@ public class VariantRestControllers {
 			response.setMessage(message);
 			return response;
 		}
-		
-		
-		
+				
+	}
+	
+	
+	@GetMapping("/getByCategoryId")
+	public Resp<List<VariantDTO>> getVariantsByCategoryId(@RequestParam("category_id") Long categoryId) {
+		// Mengemas Response API
+		int code = 200;
+		String message = "Sukses";
+		List<VariantDTO> data = vs.getVariantsByCategoryId(categoryId);
+
+		Resp<List<VariantDTO>> response = new Resp<>();
+		response.setCode(code);
+		response.setMessage(message);
+		response.setData(data);
+
+		return response;
 	}
 	
 }
