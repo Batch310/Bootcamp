3293f6d24a09eab772e10ec38d7c7e9bac32af5f
arnandasw <arnandawibawa1114@gmail.com>
2023-01-19 09:24:45 +0700

    pos

--- Nanda/POS_USER/src/main/resources/static/menu/get-menu.js
+++ Nanda/POS_USER/src/main/resources/static/menu/get-menu.js
@@ -1,4 +1,3 @@
-//------------- GET ALL CATEGORIES -------------------------
 function getAllCategoriesApi() {
 	return $.ajax({
 		url: "/api/category/get",
@@ -15,50 +14,3 @@ function searchCategoriesApi(keyword){
 		async: false
 	});
 }
-
-function refreshList(keyword) {
-	
-	if(keyword == null){
-		keyword = "";
-	}
-	
-	var response = searchCategoriesApi(keyword).responseJSON;
-	console.log(response);
-
-	var list = response.data;
-
-	$("#content-tbody").html("");
-	for (i = 0; i < list.length; i++) {
-		$("#content-tbody").append(
-			`
-			<tr>
-			<td>${list[i].initial}</td>
-			<td>${list[i].name}</td> 
-			<td><input type="checkbox" disabled 
-				${list[i].active == true ? 'checked' : ''}></td>
-			<td>
-				<button class="btn btn-warning" 
-				onclick="bukaPopupEdit(
-						'${list[i].initial}',
-						'${list[i].name}',
-						${list[i].active},
-						${list[i].id})">
-					<i class="fa-solid fa-pen-to-square" color="white"></i>
-				</button>
-				<button class="btn btn-danger" 
-				onclick="bukaPopupDelete(
-						'${list[i].initial}',
-						'${list[i].name}',
-						${list[i].active},
-						${list[i].id})">
-					<i class="fa-solid fa-trash" color="white"></i>
-				</button>
-			</td>
-			</tr>
-			`
-		)
-	}
-}
-
-refreshList();
-//----------------------------------------------------------
