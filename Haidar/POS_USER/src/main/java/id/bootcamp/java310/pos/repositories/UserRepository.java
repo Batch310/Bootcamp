1613e84b7ba99d44a615911a4f1a348f5efa22f0
hamirf <haidar300400@gmail.com>
2023-01-19 17:02:06 +0700

    2

--- Haidar/POS_USER/src/main/java/id/bootcamp/java310/pos/repositories/UserRepository.java
+++ Haidar/POS_USER/src/main/java/id/bootcamp/java310/pos/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package id.bootcamp.java310.pos.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +17,13 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	public UserDTO login(
 			@Param("email") String email, 
 			@Param("password") String password);
+	
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "update m_user\r\n"
+			+ "set email = :email, password = :password\r\n"
+			+ "where id = :user_id")
+	public void updateEmailPassword(@Param("email") String email,
+			@Param("password") String password,
+			@Param("user_id") Long userId);
 }
--- Haidar/POS_USER/src/main/java/id/bootcamp/java310/pos/services/UserService.java
+++ Haidar/POS_USER/src/main/java/id/bootcamp/java310/pos/services/UserService.java
@@ -3,7 +3,9 @@ package id.bootcamp.java310.pos.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import id.bootcamp.java310.pos.dto.ProfileDTO;
 import id.bootcamp.java310.pos.dto.UserDTO;
+import id.bootcamp.java310.pos.repositories.BiodataRepository;
 import id.bootcamp.java310.pos.repositories.UserRepository;
 
 @Service
@@ -12,9 +14,25 @@ public class UserService {
 	@Autowired
 	private UserRepository ur;
 	
+	@Autowired
+	private BiodataRepository br; 
+		
 	public UserDTO login(String email, String password) {
 		UserDTO dataUser = ur.login(email, password);
 		return dataUser;
+	}
+	
+	public void updateBiodata(ProfileDTO dto) {
+		
+		//Save email dan password pada tabel m_user (Berdasarkan query pada UserRepostory)
+		ur.updateEmailPassword(dto.getEmail(), dto.getPassword(), dto.getUser_id());
+		
+		//Ambil biodata_id berdasarkan user_id
+		Long biodataId = br.getBiodataIdByUserId(dto.getUser_id());
+		
+		//Save Name & Phone pada tabel biodata (Berdasarkan query pada BiodataRepostory)
+		br.updateNamePhone(dto.getFullname(), dto.getPhone(), biodataId);
+		
 	}
 	
 }
