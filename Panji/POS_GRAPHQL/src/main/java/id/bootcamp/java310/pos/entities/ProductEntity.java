14e74c63177309d979a4e3d70e9d1c006d092c70
Panji Tektok <panjibernadinus@gmail.com>
2023-01-23 16:53:20 +0700

    tek day 10

--- Panji/POS_GRAPHQL/src/main/java/id/bootcamp/java310/pos/entities/ProductEntity.java
+++ Panji/POS_GRAPHQL/src/main/java/id/bootcamp/java310/pos/entities/ProductEntity.java
@@ -3,79 +3,28 @@ package id.bootcamp.java310.pos.entities;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.ColumnResult;
-import javax.persistence.ConstructorResult;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedNativeQueries;
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.SqlResultSetMapping;
-import javax.persistence.SqlResultSetMappings;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import id.bootcamp.java310.pos.dto.ProductDTO;
-
-@NamedNativeQueries(value = {
-		@NamedNativeQuery(
-				name = "get_product_with_pagination",
-				query = "SELECT V.CATEGORY_ID,\r\n"
-						+ "	P.VARIANT_ID,\r\n"
-						+ "	P.ID,\r\n"
-						+ "	C.NAME AS CATEGORY_NAME,\r\n"
-						+ "	V.NAME AS VARIANT_NAME,\r\n"
-						+ "	P.INITIAL,\r\n"
-						+ "	P.NAME,\r\n"
-						+ "	P.ACTIVE,\r\n"
-						+ "	P.DESCRIPTION,\r\n"
-						+ "	P.PRICE,\r\n"
-						+ "	P.STOCK\r\n"
-						+ "FROM VARIANT V\r\n"
-						+ "INNER JOIN CATEGORY C\r\n"
-						+ "	ON C.ID = V.CATEGORY_ID\r\n"
-						+ "INNER JOIN PRODUCT P\r\n"
-						+ "	ON V.ID = P.VARIANT_ID\r\n"
-						+ "WHERE P.IS_DELETE = FALSE\r\n"
-						+ "ORDER BY ID ASC\r\n"
-						+ "LIMIT :LIMIT\r\n"
-						+ "OFFSET :OFFSET",
-				resultSetMapping = "product_results")
-})
-@SqlResultSetMappings(value = {
-		@SqlResultSetMapping(name = "product_results",
-				classes = @ConstructorResult(
-						targetClass = ProductDTO.class,
-						columns = {
-								@ColumnResult(name = "category_id",type = Long.class),
-								@ColumnResult(name = "Variant_id",type = Long.class),
-								@ColumnResult(name = "id",type = Long.class),
-								@ColumnResult(name = "category_name",type = Long.class),
-								@ColumnResult(name = "variant_name",type = Long.class),
-								@ColumnResult(name = "initial",type = Long.class),
-								@ColumnResult(name = "name",type = Long.class),
-								@ColumnResult(name = "active",type = Long.class),
-								@ColumnResult(name = "description",type = Long.class),
-								@ColumnResult(name = "price",type = Long.class),
-								@ColumnResult(name = "stock",type = Long.class)		
-						}))
-})
 @Entity // Menandakan class CategoryEntity itu Entity
 @Table(name = "product") // Menamakan Tabel
-public class ProductEntity extends BaseProperties {
+public class ProductEntity {
 	@Id // Primary Key
 	@Column(nullable = false) // Kolom gk boleh null
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment
 	private Long id;
-
+	
 	@ManyToOne
-	@JoinColumn(name = "variant_id", insertable = false, updatable = false)
+	@JoinColumn(name="variant_id", insertable = false, updatable = false)
 	private VariantEntity variantEntity;
-
+	
 	@Column(name = "variant_id", nullable = false)
 	private Long variantId;
 
@@ -84,25 +33,39 @@ public class ProductEntity extends BaseProperties {
 
 	@Column(length = 50, nullable = false, unique = true)
 	private String name;
-
+	
 	@Column(length = 500, nullable = true)
 	private String description;
-
+	
 	@Column(columnDefinition = "Decimal(18,2)", nullable = false)
 	private Double price;
-
+	
 	@Column(columnDefinition = "Decimal(18,2)", nullable = false)
 	private Double stock;
 
 	@Column(nullable = false)
 	private Boolean active;
 
+	@Column(name = "create_by", length = 50, nullable = false)
+	private String createBy;
+
+	@Column(nullable = false, name="create_date")
+	@JsonFormat(pattern = "dd-MM-yyyy HH:mm",timezone = "Asia/Jakarta")
+	private Date createDate;
+
+	@Column(name = "modify_by", length = 50)
+	private String modifyBy;
+
+	@Column(name="modify_date")
+	@JsonFormat(pattern = "dd-MM-yyyy HH:mm",timezone = "Asia/Jakarta")
+	private Date modifyDate;
+	
 	public ProductEntity() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public ProductEntity(Long id, Long variantId, String initial, String name, String description, Double price,
-			Double stock, Boolean active) {
+			Double stock, Boolean active, String createBy, Date createDate) {
 		super();
 		this.id = id;
 		this.variantId = variantId;
@@ -112,7 +75,10 @@ public class ProductEntity extends BaseProperties {
 		this.price = price;
 		this.stock = stock;
 		this.active = active;
+		this.createBy = createBy;
+		this.createDate = createDate;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -184,5 +150,37 @@ public class ProductEntity extends BaseProperties {
 
 	public void setActive(Boolean active) {
 		this.active = active;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getModifyBy() {
+		return modifyBy;
+	}
+
+	public void setModifyBy(String modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
 	}
 }
