9813e9de6395904d511c50fea9842936f2f7912c
Afdhol <afdholbasil@gmail.com>
2023-01-24 09:18:34 +0700

    pp

---
+++ Afdhol/POS_USER_JWT_SECURITY/src/main/java/id/bootcamp/java310/pos/controllers/VariantController.java
@@ -0,0 +1,35 @@
+package id.bootcamp.java310.pos.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variant")
+public class VariantController {
+
+	@RequestMapping("/home")
+	public String home(HttpServletRequest request, Model model) {
+
+		String roleCode = (String) request.getSession().getAttribute("role_code");
+		Long userId = (Long) request.getSession().getAttribute("user_id");
+
+		//oper role code
+		model.addAttribute("user_id",userId);
+		
+		if (roleCode == null) {
+			return "redirect:/login";
+		}else if(roleCode.equals("ADMIN") || roleCode.equals("WAREHOUSE")) {
+			return "variant/variant-pisah.html";
+		}
+		else {
+			return "redirect:/user/home";
+		}
+		
+		// return "category/category-template.html";
+//		return "category/category-full.html";
+	}
+
+}
