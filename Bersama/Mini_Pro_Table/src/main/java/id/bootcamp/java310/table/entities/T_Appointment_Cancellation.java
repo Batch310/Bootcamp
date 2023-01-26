92c24cfe7533ada8f113223674d1eec94c36c074
Imam Aditya <120548261+ImamAditya@users.noreply.github.com>
2023-01-26 16:52:33 +0700

    MiniPro

---
+++ Bersama/Mini_Pro_Table/src/main/java/id/bootcamp/java310/table/entities/T_Appointment_Cancellation.java
@@ -0,0 +1,41 @@
+package id.bootcamp.java310.table.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_appointment_cancellation")
+public class T_Appointment_Cancellation extends BaseProperties {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long id;
+	
+	private Long appointment_id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getAppointment_id() {
+		return appointment_id;
+	}
+
+	public void setAppointment_id(Long appointment_id) {
+		this.appointment_id = appointment_id;
+	}
+	
+	
+	
+
+
+}
