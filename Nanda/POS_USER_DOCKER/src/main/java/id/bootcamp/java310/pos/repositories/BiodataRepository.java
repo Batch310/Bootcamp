efcc8be606a75ba96399e3212373345d7071809e
arnandasw <arnandawibawa1114@gmail.com>
2023-01-26 13:41:57 +0700

    tech

---
+++ Nanda/POS_USER_DOCKER/src/main/java/id/bootcamp/java310/pos/repositories/BiodataRepository.java
@@ -0,0 +1,43 @@
+package id.bootcamp.java310.pos.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import id.bootcamp.java310.pos.dto.ProfileDTO;
+import id.bootcamp.java310.pos.entities.BiodataEntity;
+
+@Repository
+public interface BiodataRepository extends JpaRepository<BiodataEntity, Long>{
+	
+	@Query(nativeQuery = true, name = "profile_query" )
+	public ProfileDTO getProfile(@Param("user_id") Long id);
+	
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "update biodata\r\n"
+			+ "set image_path = :image_path\r\n"
+			+ "where id = :biodata_id")
+	public void upload(@Param("image_path") String imagePath, @Param("biodata_id") Long biodataId);
+	
+	@Query(nativeQuery = true, value = "select biodata_id from m_user where id = :user_id")
+	public Long getBiodataIdFromUserId(@Param("user_id") Long userId);
+	
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "update biodata set fullname = :name, mobile_phone = :phone where id = :biodata_id")			
+	public void userData(@Param("name") String name, @Param("phone") String phone, @Param("biodata_id") Long biodataId);
+
+	@Query(nativeQuery = true, value = "insert into biodata\r\n"
+			+ "(fullname, created_by, created_on)\r\n"
+			+ "values\r\n"
+			+ "(:name, 1, now()) returning id")
+	public Long saveBioRegist(@Param("name") String name);
+
+
+
+}
