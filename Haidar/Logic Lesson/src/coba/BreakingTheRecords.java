8d6ef9d7c58893863d06cb13a37425a751920035
hamirf <haidar300400@gmail.com>
2023-02-21 10:16:16 +0700

    BetweenTwoSets Hackerrank Basic Done

---
+++ Haidar/Logic Lesson/src/coba/BreakingTheRecords.java
@@ -0,0 +1,15 @@
+package coba;
+
+import java.util.Scanner;
+
+public class BreakingTheRecords {
+	public static void main(String[] args) {
+		
+		try (Scanner masukan = new Scanner(System.in)) {
+			
+		}
+		
+		
+		
+	}
+}
